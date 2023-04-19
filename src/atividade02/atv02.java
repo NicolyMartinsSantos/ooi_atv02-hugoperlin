d6a19a47affb208a60e39d8974931effbb3f7924
nimartinns@gmail.com <127858073+NicolyMartinsSantos@users.noreply.github.com>
2023-04-19 20:41:03 -0300

    Implementando o condigo jokenpo em java

---
+++ src/atividade02/atv02.java
@@ -0,0 +1,37 @@
+package atividade02;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class atv02 {
+    public static void main(String[] args) {
+        int opcaoUsuario;
+        int opcaoComputador;
+        int jogarNovamente = 1;
+        Scanner teclado = new Scanner(System.in);
+        Random rand = new Random();
+
+        while (jogarNovamente == 1) {
+            System.out.println("Escolha uma opção \n1-pedra,\n2-papel,\n3-tesoura:");
+            opcaoUsuario = teclado.nextInt();
+
+            opcaoComputador = (rand.nextInt(3)) + 1;
+
+            if (opcaoUsuario == opcaoComputador) {
+                System.out.println("Empate...");
+            } else if (opcaoUsuario == 1 && opcaoComputador == 3) {
+                System.out.println("Usuário ganhou!!");
+            } else if (opcaoUsuario == 2 && opcaoComputador == 1) {
+                System.out.println("Usuário ganhou!!");
+            } else if (opcaoUsuario == 3 && opcaoComputador == 2) {
+                System.out.println("Usuário ganhou!!");
+            } else {
+                System.out.println("Computador ganhou!!");
+            }
+
+            System.out.println("Deseja jogar novamente? (1-sim;0-não)");
+            jogarNovamente = teclado.nextInt();
+        }
+    }
+
+}
